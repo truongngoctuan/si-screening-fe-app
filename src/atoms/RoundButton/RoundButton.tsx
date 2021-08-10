92974bbdc2f9ad38d2b9e289ac081c63810a3a7d
TRUONG Ngoc Tuan <tuan.truong@partners.3plearning.com>
2021-08-10 13:29:56 +0700

    init app from boilerplate

---
+++ src/atoms/RoundButton/RoundButton.tsx
@@ -0,0 +1,11 @@
+
+import styles from './RoundButton.module.css';
+
+export function RoundButton() {
+
+  return (
+    <div className={styles.button}>
+      
+    </div>
+  );
+}
---
+++ src/pages/game-4-buttons/Game4Buttons.tsx
@@ -0,0 +1,10 @@
+import { RoundButton } from "../../atoms/RoundButton/RoundButton";
+
+export function Game4Buttons() {
+
+  return (
+    <div>
+      <RoundButton />
+    </div>
+  );
+}
